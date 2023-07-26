9e1a77976bf846e4cbc8f9009376ec39f05bf8d6
Alex Bauer <114169614+bauera-cloud@users.noreply.github.com>
2023-07-26 11:41:30 -0400

    Add addBookToLibrary(), createBook(), and css
    Now there's functionality to create a new book using the Object
    constructor for the library.
    
    There's now HTML/CSS for an option to delete a book as well.

--- index.js
+++ index.js
@@ -6,62 +6,73 @@ let myLibrary = [
         read: true
     },
     {
-        title: "Death's end",
+        title: "Death's End",
         author: "Liu Cixin",
         cover: 'assets/images/deaths_end.jpg',
         read: false
-    },
+    }
 ];
 
 
 function displayLibrary() {
     myLibrary.forEach((libraryBook) => {
-        let library = document.querySelector('#library');
-        let book = document.createElement('div');
-        book.setAttribute('class', 'book');
-
-        let cover = document.createElement('div');
-        cover.setAttribute('class', 'cover');
-        let image = document.createElement('img');
-        image.setAttribute('src', libraryBook.cover);
-
-        let description = document.createElement('div');
-        description.setAttribute('class', 'description');
-        let title = document.createElement('div');
-        title.setAttribute('class', 'title');
-        title.textContent = libraryBook.title;
-        let author_read = document.createElement('div');
-        author_read.setAttribute('class', 'author_read');
-        let author = document.createElement('span');
-        author.setAttribute('class', 'author');
-        author.textContent = libraryBook.author;
-        let checkMark = document.createElement('span');
-        checkMark.setAttribute('class', 'material-symbols-outlined read-checkmark');
-        checkMark.textContent = 'check';
-
-        if (!libraryBook.read) {
-            checkMark.style.visibility = 'hidden'
-        }
-
-        library.appendChild(book);
-
-        book.appendChild(cover);
-        cover.appendChild(image);
-
-        book.appendChild(description);
-        description.appendChild(title);
-        description.append(author_read);
-        author_read.appendChild(author);
-        author_read.appendChild(checkMark);
+        createBook(libraryBook)
     })
+}
 
+function createBook(libraryBook) {
+    let library = document.querySelector('#library');
+    let book = document.createElement('div');
+    book.setAttribute('class', 'book');
+
+    let closeX = document.createElement('span');
+    closeX.setAttribute('class', 'material-symbols-outlined');
+    closeX.setAttribute('id', 'x');
+    closeX.textContent = 'close';
+
+    let cover = document.createElement('div');
+    cover.setAttribute('class', 'cover');
+    let image = document.createElement('img');
+    image.setAttribute('src', libraryBook.cover);
+
+    let description = document.createElement('div');
+    description.setAttribute('class', 'description');
+    let title = document.createElement('div');
+    title.setAttribute('class', 'title');
+    title.textContent = libraryBook.title;
+    let author_read = document.createElement('div');
+    author_read.setAttribute('class', 'author_read');
+    let author = document.createElement('span');
+    author.setAttribute('class', 'author');
+    author.textContent = libraryBook.author;
+    let checkMark = document.createElement('span');
+    checkMark.setAttribute('class', 'material-symbols-outlined read-checkmark');
+    checkMark.textContent = 'check';
+
+    if (!libraryBook.read) {
+        checkMark.style.visibility = 'hidden'
+    }
+
+    library.appendChild(book);
+
+    book.appendChild(closeX);
+
+    book.appendChild(cover);
+    cover.appendChild(image);
+
+    book.appendChild(description);
+    description.appendChild(title);
+    description.append(author_read);
+    author_read.appendChild(author);
+    author_read.appendChild(checkMark);
 }
 
 function Book(title, author, cover, read) {
+    console.log(read)
     this.title = title
     this.author = author
     this.cover = cover
-    // this.read = read === "Yes" ? "read" : "not read yet"
+    this.read = read === 'yes' ? true : false
 }
 
 Book.prototype.info = function () {
@@ -73,15 +84,25 @@ function addBookToLibrary() {
     const form = document.querySelector('#add_book');
     const title = document.querySelector('#title');
     const author = document.querySelector('#author');
-    const read = document.querySelector('input[type="radio"]');
+    const cover = document.querySelector('#cover');
     form.addEventListener('submit', (e) => {
+        const read = document.querySelector('input[name="read"]:checked');
         e.preventDefault();
-        console.log(title, author, read)
+        let newBook = new Book(title.value, author.value, cover.value, read.value)
+        myLibrary.push(newBook)
+        createBook(newBook)
     })
 }
 
-displayLibrary()
+function deleteBookFromLibrary() {
+    const closeX = document.querySelector('#x');
+    closeX.addEventListener('click', (e) => {
+
+    })
+}
+
 addBookToLibrary()
+displayLibrary()
 
 
 
